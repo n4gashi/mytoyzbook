82c28d6020f500683bf7124024debd2c29cba405
amram <david.amram.info@gmail.com>
2015-07-06 22:23:22 +0200

    Test connectivité

--- app/src/main/java/toyzdudes/mytoyzbook/helpers/FeaturesHelper.java
+++ app/src/main/java/toyzdudes/mytoyzbook/helpers/FeaturesHelper.java
@@ -5,7 +5,7 @@ import android.net.ConnectivityManager;
 
 public class FeaturesHelper {
 
-    public boolean isNetworkAvailable(Context context) {
+    public static boolean isNetworkAvailable(Context context) {
         ConnectivityManager cm = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
--- app/src/main/java/toyzdudes/mytoyzbook/workers/MTBRestClient.java
+++ app/src/main/java/toyzdudes/mytoyzbook/workers/MTBRestClient.java
@@ -1,15 +1,25 @@
 package toyzdudes.mytoyzbook.workers;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import com.loopj.android.http.*;
+
+import toyzdudes.mytoyzbook.helpers.DebugHelper;
+import toyzdudes.mytoyzbook.helpers.FeaturesHelper;
 
 public class MTBRestClient {
 
     private static final String BASE_URL = "http://mytoyzbook.alwaysdata.net/webservice/";
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler)
+    public static void get(Context context, String url, RequestParams params, AsyncHttpResponseHandler responseHandler)
     {
-        client.get(getAbsoluteUrl(url), params, responseHandler);
+        if(FeaturesHelper.isNetworkAvailable(context)){
+            client.get(getAbsoluteUrl(url), params, responseHandler);
+        }else{
+            Toast.makeText(context, "Probleme de connexion", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private static String getAbsoluteUrl(String relativeUrl)
